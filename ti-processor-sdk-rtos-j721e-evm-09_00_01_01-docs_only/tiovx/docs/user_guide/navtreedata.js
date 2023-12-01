de2ec3dfe5500b6459459e647c6317531d0015ca
James <james237.wang@gmail.com>
2023-12-02 01:49:31 +0800

    fix rtos sdk docs no file problem

---
+++ ti-processor-sdk-rtos-j721e-evm-09_00_01_01-docs_only/tiovx/docs/user_guide/navtreedata.js
@@ -0,0 +1,64 @@
+/*
+@ @licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2017 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "TIOVX User Guide", "index.html", [
+    [ "Overview", "index.html", [
+      [ "Introduction", "index.html#autotoc_md7", null ],
+      [ "Directory Structure", "index.html#TIOVX_PACKAGE_CONTENTS", null ]
+    ] ],
+    [ "User Resources", "REFERENCES.html", "REFERENCES" ],
+    [ "Resources", "RESOURCES.html", [
+      [ "Khronos Pages", "RESOURCES.html#autotoc_md0", null ],
+      [ "Specs", "RESOURCES.html#autotoc_md1", null ],
+      [ "Tutorials/Presentations", "RESOURCES.html#autotoc_md2", null ],
+      [ "Papers", "RESOURCES.html#autotoc_md3", null ],
+      [ "FAQ's", "RESOURCES.html#autotoc_md4", null ],
+      [ "TI Support", "RESOURCES.html#autotoc_md5", null ]
+    ] ],
+    [ "TI Disclaimer", "TI_DISCLAIMER.html", [
+      [ "IMPORTANT NOTICE", "TI_DISCLAIMER.html#autotoc_md6", null ]
+    ] ],
+    [ "APIs", "modules.html", "modules" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"BUILD_INSTRUCTIONS.html",
+"classtiovx_1_1enums_1_1InterpolationType.html#a10ccb46377ffbca096ae711789c1219f",
+"classtiovx_1_1node_1_1NodeIntegralImage.html#a5d679bb9d4757e99745cf234118c5d22",
+"group__group__tivx__ext__host.html#gac58b060af2238765f0d152c626fd06c7",
+"group__group__tivx__int__common__kernel.html#ga6659fa78d5c7514c2a98ef914cd5c717",
+"group__group__tivx__obj__desc__priv.html#gae0cdebf83777a62372c132e9dcafab81",
+"group__group__vision__function__vpac__ldc.html",
+"group__group__vx__graph__cfg.html#ga1e90c432483f28c0c32b249577b46dde",
+"group__group__vx__ti__extensions__cfg.html#gaa020e2b6ed4b2b168bcfecb1f52eba86",
+"structtivx__graph__t.html#a22af52a9ecb54e936dbabae54db5d9aa",
+"structtivx__obj__desc__node__t.html#a7d11ec9df23037c22ec5b35e6581f7c9",
+"structtivx__shared__mem__ptr__t.html#a5b4f3a394d98a8d13566f247c0d8ebd1"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
